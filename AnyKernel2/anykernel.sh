00e2c9dd738ca6148461fa34bfc14b6a0b2ab3de
Vagelis1608 <vagelis1608@gmail.com>
2017-07-31 12:35:06 +0300

    Added AnyKernel2 template in the repo (AnyKernel2 developed by osm0sis @ xda-developers) This has been added for later RAMDisk-based mods, which are applied in the template.

---
+++ AnyKernel2/anykernel.sh
@@ -0,0 +1,43 @@
+# AnyKernel2 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() {
+kernel.string=-V- Kernel X v1.0 by Vagelis1608 @ xda-developers
+do.devicecheck=1
+do.modules=0
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=g2m
+device.name2=d620
+device.name3=d620r
+device.name4=d618
+device.name5=d610
+} # end properties
+
+# shell variables
+block=/dev/block/platform/msm_sdcc.1/by-name/boot;
+is_slot_device=0;
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. /tmp/anykernel/tools/ak2-core.sh;
+
+
+## AnyKernel permissions
+# set permissions for included ramdisk files
+chmod -R 755 $ramdisk
+
+## AnyKernel install
+dump_boot;
+
+# begin ramdisk changes
+
+# end ramdisk changes
+
+write_boot;
+
+## end install
+
