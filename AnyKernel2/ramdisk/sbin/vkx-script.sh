23124aac48b51ddb70ce5edf29011e28a371b0f1
Vagelis1608 <vagelis1608@gmail.com>
2017-08-17 13:34:20 +0300

    Added MPDecision force stop script This checks the (new!) kernel's properties file (/data/vkx.prop) for: persist.mpdecision.stop and if it's set to 1 will force stop MPDecision Setting it to 0 will let MPDecision running and reset any changes done. (Specifically, renaming the mpdecision binary from mpdecision-dis in /system/bin)

--- AnyKernel2/ramdisk/sbin/vkx-script.sh
+++ AnyKernel2/ramdisk/sbin/vkx-script.sh
@@ -3,5 +3,43 @@
 # -V- Kernel X main script file
 # Written for -V- Kernel X by Vagelis1608 @xda-developers
 
-# Placeholder #
+# Variables
+# Busybox build by osm0sis @xda-developers
+BB=/sbin/bb/busybox
+PROP=/data/vkx.prop
+
+# Create the properties file, if missing, then set permissions/owner.
+if [ ! -e $PROP ]; then
+    touch $PROP
+    echo "# Properties file for -V- Kernel X" > $PROP
+    echo "" >> $PROP
+fi
+chmod 644 $PROP
+chown root.root $PROP
+
+# Grab properties
+gprop() { $BB grep "^$1" "$PROP" | $BB cut -d= -f2; }
+
+MPDS=`gprop persist.mpdecision.stop`
+if [ -z $MPDS ]; then
+    echo "persist.mpdecision.stop=1" >> $PROP
+    MPDS=1
+fi
+
+# Force stop MPDecision if the prop persist.mpdecision.stop is set to 1
+if [ "$MPDS" == "1" ]; then
+    setprop ctl.stop mpdecision
+    stop mpdecision
+    if [ -e /system/bin/mpdecision ]; then
+        mount -t auto -o rw,remount /system
+        $BB cp -af /system/bin/mpdecision /system/bin/mpdecision-dis
+        $BB rm -f /system/bin/mpdecision
+        mount -t auto -o ro,remount /system
+    fi
+elif [ "$MPDS" == "0" -a -e /system/bin/mpdecision-dis ]; then
+        mount -t auto -o rw,remount /system
+        $BB cp -af /system/bin/mpdecision-dis /system/bin/mpdecision
+        $BB rm -f /system/bin/mpdecision-dis
+        mount -t auto -o ro,remount /system        
+fi
 
